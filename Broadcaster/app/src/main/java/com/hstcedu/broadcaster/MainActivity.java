9872344044d2e27529b394632bd9a1ce32da3a21
rohero6 <vipluojunjie@126.com>
2019-10-21 00:00:45 +0800

    使用intent隐式召唤浏览器，电话，地图

---
+++ Broadcaster/app/src/main/java/com/hstcedu/broadcaster/MainActivity.java
@@ -0,0 +1,50 @@
+package com.hstcedu.broadcaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button browser=findViewById(R.id.browser);
+        Button call = findViewById(R.id.call);
+        Button map = findViewById(R.id.map);
+        browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("tel:15992053725"));
+                startActivity(intent);
+            }
+        });
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:39.56，116.23"));
+                startActivity(intent);
+            }
+        });
+    }
+}
