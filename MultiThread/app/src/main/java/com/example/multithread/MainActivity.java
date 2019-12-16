520012bffae29f122585f3c933247ed4dc41ea2c
rohero6 <vipluojunjie@126.com>
2019-12-16 12:47:34 +0800

    多线程处理

---
+++ MultiThread/app/src/main/java/com/example/multithread/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.multithread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int counter=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt = findViewById(R.id.change_text);
+        final TextView tx = findViewById(R.id.textview);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DemoThread thread1 = new DemoThread();
+                Log.d("子线程号", "" + thread1.getId());
+                try {
+                    thread1.start();  //开始休眠5秒
+
+                } catch (Exception e) {
+
+                }
+                Log.d("主线程号", "" + Thread.currentThread().getId());
+                tx.setText("我是" + counter);
+            }
+
+
+        });
+    }
+
+        class DemoThread extends Thread {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+
+                }
+                counter++;
+
+            }
+        }
+    }
