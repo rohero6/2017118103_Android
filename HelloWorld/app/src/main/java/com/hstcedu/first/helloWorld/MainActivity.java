37e04869e9b9f8270283c66633d1b1f02f16cb7d
rohero6 <vipluojunjie@126.com>
2020-01-04 15:27:59 +0800

    no message

--- HelloWorld/app/src/main/java/com/hstcedu/first/helloWorld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/hstcedu/first/helloWorld/MainActivity.java
@@ -1,11 +1,11 @@
 package com.hstcedu.first.helloWorld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this,SecondActivity.class);
                 startActivity(intent);
+
+
+                
             }
         });
 
