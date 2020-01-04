04e50a36ed60aadea50ce4d08437a0d900f01ee1
rohero6 <vipluojunjie@126.com>
2020-01-04 15:27:24 +0800

    no message

--- HelloWorld/app/src/main/java/com/hstcedu/first/helloWorld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/hstcedu/first/helloWorld/MainActivity.java
@@ -1,11 +1,11 @@
 package com.hstcedu.first.helloWorld;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button bt1= findViewById(R.id.bt1);
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(intent);
-            }
-        });
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
